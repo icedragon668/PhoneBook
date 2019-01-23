a1803b252693828cf701b5357bd3aaa72789091c
Kori <icedragon668@gmail.com>
2019-01-23 18:59:50 -0500

    bulk push, semi-functional. it works once

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 
 // functions //
 const render = function () {
-    for (let i = 0; i < employeeList.length; i++) { //stick the out into a 'card' div for style?
-        $('.cards').append(`<div id="${employeeList[i].name}Card"><p>Name: ${employeeList[i].name}</p>
-                      <p>Office Number: ${employeeList[i].officeNum}</p>
-                      <p> Phone Number: ${employeeList[i].phoneNum}</p></div>`)
-    } //renders the list, with DOM style options  
+    $(".cards").empty();
+    for (let i = 0; i < employeeListJS.length; i++) { //stick the out into a 'card' div for style?
+    // console.log(`${employeeList[employeeIndex].name} take 3`);
+        $('.cards').append(`<div id="${employeeListJS[i].name}" class="card"><p>Name: ${employeeListJS[i].name}</p>
+                      <p>Office Number: ${employeeListJS[i].officeNum}</p>
+                      <p> Phone Number: ${employeeListJS[i].phoneNum}</p></div>`)
+    } // console.log(`${employeeList[employeeIndex].name} take 4`); //renders the list, with DOM style options  
+    const employeeNameArray = [];
+    for (let i = 0; i < employeeListJS.length; i++) {
+        employeeNameArray.push(employeeListJS[i].name);
+        console.log(employeeNameArray);
+    }
 }  
 
 const formAdd = function () {
@@ -18,29 +25,75 @@ const formAdd = function () {
         officeNum: officeNum,
         phoneNum: phoneNum
     } // end #$%^ING OBJECT
-    employeeList.push(newEmployee);
+    employeeListJS.push(newEmployee);
     render();
 };
-const formDelete = function () { console.log(`Placeholder!`) } // .EMPTY
-const formUpdate = function () { console.log(`Placeholder!`) } // .TEXT
+
+const formDelete = function () { //deleting at index number of original name, no updating
+    console.log(document.forms["deleteForm"]["name"].value);  // ie Juan deletes [1] everytime
+    console.log(employeeNameArray);
+    for (let i = 0; i < employeeNameArray.length; i++) {
+        const element = employeeNameArray[i];
+        if (document.forms["deleteForm"]["name"].value === element) {
+            employeeIndex = i;
+        }
+    }
+    if (employeeIndex >= 0) {
+        console.log("Present");
+        console.log(employeeIndex);
+        employeeListJS.splice(employeeIndex, 1);
+    } else {
+        console.log("Absent");
+    }
+
+    render();
+    employeeIndex = -1;
+}
+
+
+const formUpdate = function () {
+    console.log(document.forms["updateForm"]["oldName"].value);
+
+    const oldName = document.forms["updateForm"]["oldName"].value;
+    const newName = document.forms["updateForm"]["name"].value;
+    const officeNum = document.forms["updateForm"]["officeNum"].value;
+    const phoneNum = document.forms["updateForm"]["phoneNum"].value;
+
+    for (let i = 0; i < employeeNameArray.length; i++) {
+        const element = employeeNameArray[i];
+        if (document.forms["updateForm"]["oldName"].value === element) {
+            employeeIndex = i;
+        }
+    }
+    if (employeeIndex >= 0) {
+        console.log("Present");
+        console.log(employeeIndex);
+        employeeListJS[employeeIndex].name = newName
+        console.log(employeeListJS[employeeIndex].name);
+    } else {
+        console.log("Absent");
+    }
+    // console.log(`${employeeListJS[employeeIndex].name} take 2`);
+    render();
+    employeeIndex = -1;
+} // .TEXT
+
 const formVerify = function () {
-    console.log(`Placeholder!`)
     console.log(employeeNameArray);
     for (let i = 0; i < employeeNameArray.length; i++) {
         const element = employeeNameArray[i];
         if (document.forms["verifyForm"]["name"].value === element) {
-            console.log(`Present!`);
-        } else { }
+            employeeIndex = i;
+        }
     }
-
-    //psuedocode
-    /*
-    get namelist and place in array
-    check array for nameinput
-   inout name, check name v employeelist... add doesn/t add to employee list...
-   check text/html of the cards?
-   if inputName is in .cards, then yay, else boo
-   */
+    if (employeeIndex >= 0) {
+        // console.log("Present");
+        alert("Present");
+        console.log(employeeIndex);
+    } else {
+        console.log("Absent");
+    }
+    employeeIndex = -1;
 }
 
 const reveal = function (which) {
@@ -87,6 +140,7 @@ const handleClick = function (event) {
 
 
 let which = 'initial';
+let employeeIndex = -1;
 const showAdd = function () { reveal("Add") }
 const showDelete = function () { reveal("Delete") }
 const showUpdate = function () { reveal("Update") }
@@ -115,7 +169,9 @@ for (let i = 0; i < employeeList.length; i++) {
 
 //this block initializes the list
 for (let i = 0; i < employeeList.length; i++) { //stick the out into a 'card' div for style
-    $('.cards').append(`<div id=${employeeList[i].name} Card><p>Name: ${employeeList[i].name}</p>
+    $('.cards').append(`<div id="${employeeList[i].name}" class="card"><p>Name: ${employeeList[i].name}</p>
                       <p>Office Number: ${employeeList[i].officeNum}</p>
                       <p> Phone Number: ${employeeList[i].phoneNum}</p></div>`)
 }  // changes this to create and object, then listing it. allows for array manipulation instead of just DOM >.<
+
+const employeeListJS = employeeList
